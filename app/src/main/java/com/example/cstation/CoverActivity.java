e391adea28dd8a434e9ada63a93e73475cacd9c4
Rifqi Putra Adhadi <rifqiputraadhadi@gmail.com>
2024-12-26 23:10:44 +0700

    finished project

--- app/src/main/java/com/example/cstation/CoverActivity.java
+++ app/src/main/java/com/example/cstation/CoverActivity.java
@@ -13,13 +13,76 @@ public class CoverActivity extends AppCompatActivity {
         // Ambil data dari Intent
         String novel = getIntent().getStringExtra("novel");
 
-        // Tentukan layout berdasarkan novel yang diklik
-        if ("Renegede Immortal".equals(novel)) {
-            setContentView(R.layout.cover1);
-        } else if ("Overgeared".equals(novel)) {
-            setContentView(R.layout.cover5);
-        } else if ("My Divine Diary".equals(novel)){
-            setContentView(R.layout.cover20);
+        switch (novel) {
+            case "Renegede Immortal":
+                setContentView(R.layout.cover1);
+                break;
+            case "Alchemy Emperor Of The Divine Dao":
+                setContentView(R.layout.cover2);
+                break;
+            case "Martial Peak":
+                setContentView(R.layout.cover3);
+                break;
+            case "Nine Star Hegemon Body Art":
+                setContentView(R.layout.cover4);
+                break;
+            case "Overgeared":
+                setContentView(R.layout.cover5);
+                break;
+            case "Solo Farming In The Tower":
+                setContentView(R.layout.cover6);
+                break;
+            case "Pick Me Up !":
+                setContentView(R.layout.cover7);
+                break;
+            case "The Imperial Hunter":
+                setContentView(R.layout.cover8);
+                break;
+            case "The Return Of The Legendary All-Master":
+                setContentView(R.layout.cover9);
+                break;
+            case "Genius Game Broadcaster":
+                setContentView(R.layout.cover10);
+                break;
+            case "The Archmage's Restaurant":
+                setContentView(R.layout.cover11);
+                break;
+            case "Spy Kyoutshitsu":
+                setContentView(R.layout.cover12);
+                break;
+            case "Unnamed Memory":
+                setContentView(R.layout.cover13);
+                break;
+            case "Mushoku Tensei":
+                setContentView(R.layout.cover14);
+                break;
+            case "The Last Adventurer":
+                setContentView(R.layout.cover15);
+                break;
+            case "Isekai Nonbiri Nouka":
+                setContentView(R.layout.cover16);
+                break;
+            case "Konjiki no Moji Tsukai":
+                setContentView(R.layout.cover17);
+                break;
+            case "Star Rank Hunter":
+                setContentView(R.layout.cover18);
+                break;
+            case "Outside of Time":
+                setContentView(R.layout.cover19);
+                break;
+            case "My Divine Diary":
+                setContentView(R.layout.cover20);
+                break;
+            case "Mysterious Noble Beasts":
+                setContentView(R.layout.cover21);
+                break;
+            case "Mechanical God Emperor":
+                setContentView(R.layout.cover22);
+                break;
+            case "Archean Eon Art":
+                setContentView(R.layout.cover23);
+                break;
         }
 
         // Mengaktifkan tombol back di toolbar
--- app/src/main/java/com/example/cstation/MainActivity.java
+++ app/src/main/java/com/example/cstation/MainActivity.java
@@ -31,8 +31,28 @@ public class MainActivity extends AppCompatActivity {
         searchInput = findViewById(R.id.searchInput);
         searchResult = findViewById(R.id.searchResult);
         ImageView cover1 = findViewById(R.id.cover1);
+        ImageView cover2 = findViewById(R.id.cover2);
+        ImageView cover3 = findViewById(R.id.cover3);
+        ImageView cover4 = findViewById(R.id.cover4);
         ImageView cover5 = findViewById(R.id.cover5);
+        ImageView cover6 = findViewById(R.id.cover6);
+        ImageView cover7 = findViewById(R.id.cover7);
+        ImageView cover8 = findViewById(R.id.cover8);
+        ImageView cover9 = findViewById(R.id.cover9);
+        ImageView cover10 = findViewById(R.id.cover10);
+        ImageView cover11 = findViewById(R.id.cover11);
+        ImageView cover12 = findViewById(R.id.cover12);
+        ImageView cover13 = findViewById(R.id.cover13);
+        ImageView cover14 = findViewById(R.id.cover14);
+        ImageView cover15 = findViewById(R.id.cover15);
+        ImageView cover16 = findViewById(R.id.cover16);
+        ImageView cover17 = findViewById(R.id.cover17);
+        ImageView cover18 = findViewById(R.id.cover18);
+        ImageView cover19 = findViewById(R.id.cover19);
         ImageView cover20 = findViewById(R.id.cover20);
+        ImageView cover21 = findViewById(R.id.cover21);
+        ImageView cover22 = findViewById(R.id.cover22);
+        ImageView cover23 = findViewById(R.id.cover23);
 
         // Menambahkan TextWatcher untuk menampilkan teks yang diketik secara langsung
         searchInput.addTextChangedListener(new TextWatcher() {
@@ -76,6 +96,127 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("novel", "My Divine Diary");
             startActivity(intent);
         });
+
+        cover2.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Alchemy Emperor Of The Divine Dao");
+            startActivity(intent);
+        });
+
+        cover3.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Martial Peak");
+            startActivity(intent);
+        });
+
+        cover4.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Nine Star Hegemon Body Art");
+            startActivity(intent);
+        });
+
+        cover6.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Solo Farming In The Tower");
+            startActivity(intent);
+        });
+
+        cover7.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Pick Me Up !");
+            startActivity(intent);
+        });
+
+        cover8.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "The Imperial Hunter");
+            startActivity(intent);
+        });
+
+        cover9.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "The Return Of The Legendary All-Master");
+            startActivity(intent);
+        });
+
+        cover10.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Genius Game Broadcaster");
+            startActivity(intent);
+        });
+
+        cover11.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "The Archmage's Restaurant");
+            startActivity(intent);
+        });
+
+        cover12.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Spy Kyoutshitsu");
+            startActivity(intent);
+        });
+
+        cover13.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Unnamed Memory");
+            startActivity(intent);
+        });
+
+        cover14.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Mushoku Tensei");
+            startActivity(intent);
+        });
+
+        cover15.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "The Last Adventurer");
+            startActivity(intent);
+        });
+
+        cover16.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Isekai Nonbiri Nouka");
+            startActivity(intent);
+        });
+
+        cover17.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Konjiki no Moji Tsukai");
+            startActivity(intent);
+        });
+
+        cover18.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Star Rank Hunter");
+            startActivity(intent);
+        });
+
+        cover19.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Outside of Time");
+            startActivity(intent);
+        });
+
+        cover21.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Mysterious Noble Beasts");
+            startActivity(intent);
+        });
+
+        cover22.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Mechanical God Emperor");
+            startActivity(intent);
+        });
+
+        cover23.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+            intent.putExtra("novel", "Archean Eon Art");
+            startActivity(intent);
+        });
+
     }
 
     // Fungsi untuk mencari novel
@@ -85,21 +226,43 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        // Logika pencarian sederhana
-        if (query.equalsIgnoreCase("Renegede Immortal")) {
-            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
-            intent.putExtra("novel", "Renegede Immortal");
-            startActivity(intent);
-        } else if (query.equalsIgnoreCase("Overgeared")) {
-            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
-            intent.putExtra("novel", "Overgeared");
-            startActivity(intent);
-        } else if (query.equalsIgnoreCase("My Divine Diary")) {
-            Intent intent = new Intent(MainActivity.this, CoverActivity.class);
-            intent.putExtra("novel", "My Divine Diary");
-            startActivity(intent);
-        } else {
-            Toast.makeText(this, "Novel tidak ditemukan", Toast.LENGTH_SHORT).show();
+        // Daftar novel dan logika pencarian
+        String[] novelList = {
+                "Renegede Immortal",
+                "Alchemy Emperor Of The Divine Dao",
+                "Martial Peak",
+                "Nine Star Hegemon Body Art",
+                "Overgeared",
+                "Solo Farming In The Tower",
+                "Pick Me Up !",
+                "The Imperial Hunter",
+                "The Return Of The Legendary All-Master",
+                "Genius Game Broadcaster",
+                "The Archmage's Restaurant",
+                "Spy Kyoutshitsu",
+                "Unnamed Memory",
+                "Mushoku Tensei",
+                "The Last Adventurer",
+                "Isekai Nonbiri Nouka",
+                "Konjiki no Moji Tsukai",
+                "Star Rank Hunter",
+                "Outside of Time",
+                "My Divine Diary",
+                "Mysterious Noble Beasts",
+                "Mechanical God Emperor",
+                "Archean Eon Art"
+        };
+
+        for (String novel : novelList) {
+            if (query.equalsIgnoreCase(novel)) {
+                Intent intent = new Intent(MainActivity.this, CoverActivity.class);
+                intent.putExtra("novel", novel);
+                startActivity(intent);
+                return;
+            }
         }
+
+        Toast.makeText(this, "Novel tidak ditemukan", Toast.LENGTH_SHORT).show();
     }
+
 }
